2a9db402460a42689cd9f1fd5e530340a566fda6
theoderson-1908 <theodoredevavaram@gmail.com>
2021-07-07 16:43:20 +0530

    Puzzle

---
+++ The Puzzle/The Puzzle/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace The_Puzzle.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
